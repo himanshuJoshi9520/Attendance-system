1978f1fa5eca4a90d0cd546632fd13d5c9c5b2e8
himanshuJoshi9520 <hjoshi952080@gmail.com>
2024-05-12 14:42:42 +0530

    .gitignore file added

--- student.py
+++ student.py
@@ -537,12 +537,13 @@ class Student:
               img_id+=1
               face=cv2.resize(face_cropped(my_frame),(450,450)) 
               face=cv2.cvtColor(face,cv2.COLOR_BGR2GRAY)
+ 
               file_name_path="data/user."+str(id)+"."+str(img_id)+".jpg"
               cv2.imwrite(file_name_path,face) 
               cv2.putText(face,str(img_id),(50,50),cv2.FONT_HERSHEY_COMPLEX,2,(0,255,0),2)
               cv2.imshow("Face Cropper",face)
 
-            if cv2.waitKey(1) == 13  or int(img_id)==100:
+            if cv2.waitKey(1) == 13  or int(img_id)==10:
               break
           cap.release()  
           cv2.destroyAllWindows()
